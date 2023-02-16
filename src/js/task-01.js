73a4de77c1d9231746e38d3645b0a75cdb8cd254
Anastasiia-Berehovykh <anastanber@gmail.com>
2023-02-16 19:19:59 +0200

    structura

---
+++ src/js/task-01.js
@@ -0,0 +1,8 @@
+const numberEl = document.querySelectorAll('.item');
+
+console.log('Number of categories:', numberEl.length);
+
+const categoryEl = document.querySelectorAll('h2');
+
+console.log(categoryEl)
+//console.log('Category: ', categoryEl.textContent)
