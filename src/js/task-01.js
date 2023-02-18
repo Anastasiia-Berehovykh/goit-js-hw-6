651a3b631980652e3a30229a7655ccac31aef927
Anastasiia-Berehovykh <anastanber@gmail.com>
2023-02-18 12:10:24 +0200

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -2,9 +2,9 @@ const categoriesEl = document.getElementById('categories').children.length;
 console.log('Number of categories:', categoriesEl);
 
 const categoryItemEl = document.querySelectorAll('h2');
-categoryItemEl.forEach((item) => {
-    console.log('Category: ', item.textContent);
-    console.log('Elements: ', item.nextElementSibling.children.length);
+categoryItemEl.forEach((title) => {
+    console.log('Category: ', title.textContent);
+    console.log('Elements: ', title.nextElementSibling.children.length);
 });
     
 
